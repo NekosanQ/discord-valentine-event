c8395865dcb2c6f04ef9124a6a636595d9d32fc2
べーこんにみそ <Kinosoft@outlook.jp>
2024-02-12 20:17:22 +0900

    もらったチョコレートの個数確認

--- src/mainProcess/checkChocolate.ts
+++ src/mainProcess/checkChocolate.ts
@@ -3,8 +3,24 @@ import { Interaction } from "discord.js";
 /**
  * @param interaction インタラクション
  */
+// -----------------------------------------------------------------------------------------------------------
+// 貰ったチョコレートの個数を確認する
+// -----------------------------------------------------------------------------------------------------------
 module.exports = {
 	async execute(interaction: Interaction): Promise<void> {
+        if (!interaction.isButton()) return;
+        try {
+            await interaction.deferReply({
+                ephemeral: true
+            });
 
+            let chocolateNum: number = 0;
+            
+            await interaction.editReply({
+                content: `貰ったチョコレートは、${chocolateNum}個です。`
+            });
+        } catch(error) {
+            console.log(error);
+        }
     }
 };
