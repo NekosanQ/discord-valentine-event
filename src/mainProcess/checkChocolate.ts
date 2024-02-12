f9e1c1821aa85f5899959f6c3481e51c67d38a12
べーこんにみそ <Kinosoft@outlook.jp>
2024-02-12 22:33:36 +0900

    貰ったチョコレートの個数を確認 完成形

--- src/mainProcess/checkChocolate.ts
+++ src/mainProcess/checkChocolate.ts
@@ -1,4 +1,6 @@
 import { Interaction } from "discord.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 /**
  * @param interaction インタラクション
@@ -14,11 +16,35 @@ module.exports = {
                 ephemeral: true
             });
 
-            let chocolateNum: number = 0;
-            
-            await interaction.editReply({
-                content: `貰ったチョコレートは、${chocolateNum}個です。`
+            const user = await prisma.valentineEvent.findMany({
+                select: {
+                    user_id: true,
+                    received_count: true
+                },
+
+                where: {
+                    user_id: interaction.user.id
+                }
             });
+
+            if (user[0]) {
+                await interaction.editReply({
+                    content: `貰ったチョコレートの個数: ${user[0].received_count}個`
+                });
+            } else { // データがないので新規作成を検討します。
+                await prisma.valentineEvent.create({
+                    data: {
+                        user_id: interaction.user.id,
+                        passed_count: 0,
+                        received_count: 0,
+                        gave: false
+                    }
+                });
+
+                await interaction.editReply({
+                    content: "貰ったチョコレートの個数: 0個"
+                });
+            }
         } catch(error) {
             console.log(error);
         }
