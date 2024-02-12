96162d634fd080b7e6aabc4370dd1d362f6c557a
べーこんにみそ <Kinosoft@outlook.jp>
2024-02-12 22:43:40 +0900

    データベースに記録するようにした

--- src/mainProcess/getChocolate.ts
+++ src/mainProcess/getChocolate.ts
@@ -1,5 +1,9 @@
 import { Interaction } from "discord.js";
 import { config } from "../utils/config.js";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 /**
  * @param interaction インタラクション
  */
@@ -22,6 +26,47 @@ module.exports = {
                     chocolateNum = i + 1; // ロールの配列は0からなので1を足す
                 }
             };
+
+            const chocolate = await prisma.valentineEvent.findMany({
+                select: {
+                    gave: true
+                },
+
+                where: {
+                    user_id: interaction.user.id
+                }
+            });
+
+            if (chocolate[0]) { // 作成済み(すでに相手から受け取っているなど)
+                if (chocolate[0].gave) { // 取得済み
+                    await interaction.editReply({
+                        content: "すでにチョコレートを取得しています！"
+                    });
+
+                    return;
+                }
+
+                await prisma.valentineEvent.updateMany({
+                    where: {
+                        user_id: interaction.user.id
+                    },
+
+                    data: {
+                        passed_count: chocolateNum,
+                        gave: true
+                    }
+                });
+            } else {
+                await prisma.valentineEvent.create({
+                    data: {
+                        user_id: interaction.user.id,
+                        passed_count: chocolateNum,
+                        received_count: 0,
+                        gave: true
+                    }
+                });
+            }
+
             await interaction.editReply({
                 content: `チョコレートを${chocolateNum}個渡しました`
             });
